c060a1bd2d34ebaba0fbdbb60f2ecc37d3d5cc3d
AndreiValuika <andrei.valuika@gmail.com>
2019-11-16 00:46:34 +0300

    + first task +add tests

---
+++ NET.W.2019.Valuika.02/Numbers/Numbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers
+{
+    public class Numbers
+    {
+        public static Int32 InsertNumber(int numberSource, int numberIn, int shift,int startIndex) 
+        {
+            if (shift > startIndex)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            int size = startIndex - shift + 1;
+            int result;
+            CutNumber(ref numberIn, shift,size);
+            int mask = ~PreSource(size, shift);
+            result = (numberSource & mask) | numberIn;
+            return result;
+        }
+
+        private static void CutNumber(ref int numberIn, int shift,int size) 
+        {
+            int mask = ~0;
+            mask <<= size;
+            numberIn &= (~mask);
+            numberIn <<= shift;
+        }
+
+        private static int PreSource(int size, int shift) 
+        {
+            int mask = ~0;
+            mask <<= size;
+            mask = (~mask) << shift;
+            return mask;
+        }
+    }
+}
---
+++ NET.W.2019.Valuika.02/NumbersTests1/NumbersTestsNUnit.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Numbers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers.Tests
+{
+    [TestFixture()]
+    public class NumbersTestsNUnit
+    {
+        [TestCase(15, 15, 0, 0, ExpectedResult = 15)]
+        [TestCase(8, 15, 0, 0, ExpectedResult = 9)]
+        [TestCase(8, 15, 3, 8, ExpectedResult = 120)]
+        [TestCase(568, 53, 8, 11, ExpectedResult = 1336)]
+        public int InsertNumberTest(int numberSource, int numberIn, int i, int j)
+        {
+            return  Numbers.InsertNumber(numberSource, numberIn, i, j);
+        }
+
+        [Test]
+        public void InsertNumberTest_Ex()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Numbers.InsertNumber(11, 11, 10, 1));
+        }
+    }
+}
