c7fe4cadc397d6aa97a8a4cdf259e05e6a77f59c
AndreiValuika <andrei.valuika@gmail.com>
2019-11-16 09:22:43 +0300

    +add FindNextBiggerNumber

--- NET.W.2019.Valuika.02/Numbers/Numbers.cs
+++ NET.W.2019.Valuika.02/Numbers/Numbers.cs
@@ -8,6 +8,59 @@ namespace Numbers
 {
     public class Numbers
     {
+
+        private static int ArrayToNumber(int[] array)
+        {
+            StringBuilder tempString = new StringBuilder();
+            foreach (var item in array)
+            {
+                tempString.Append(item.ToString());
+            }
+
+            int result = Convert.ToInt32(tempString.ToString());
+            return result;
+        }
+
+        private static void SwapNumber(ref int x, ref int y) 
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public static int FindNextBiggerNumber(int number) 
+        {
+            string digitString = number.ToString();
+            int digitCount = digitString.Length;
+            int[] digitArray = new int[digitCount];
+            int index = 0;
+
+            foreach (var item in digitString)
+            {
+                digitArray[index++] = Convert.ToInt32(item.ToString());
+            }
+            int poviteIndex=0;
+
+            for (int i = digitCount-1; i>=1; i--)
+            {
+                if (digitArray[i]>digitArray[i-1])
+                {
+                    poviteIndex = i;
+                    break;
+                }
+            }
+
+            if (poviteIndex == 0)
+            {
+                return -1;
+            }
+
+            SwapNumber(ref digitArray[poviteIndex], ref digitArray[poviteIndex - 1]);
+            Array.Sort(digitArray, poviteIndex, digitCount - poviteIndex);
+
+            return ArrayToNumber(digitArray); 
+        }
+
         public static Int32 InsertNumber(int numberSource, int numberIn, int shift,int startIndex) 
         {
             if (shift > startIndex)
--- NET.W.2019.Valuika.02/NumbersTests1/NumbersTestsNUnit.cs
+++ NET.W.2019.Valuika.02/NumbersTests1/NumbersTestsNUnit.cs
@@ -17,13 +17,31 @@ namespace Numbers.Tests
         [TestCase(568, 53, 8, 11, ExpectedResult = 1336)]
         public int InsertNumberTest(int numberSource, int numberIn, int i, int j)
         {
-            return  Numbers.InsertNumber(numberSource, numberIn, i, j);
+            return Numbers.InsertNumber(numberSource, numberIn, i, j);
         }
 
         [Test]
         public void InsertNumberTest_Ex()
         {
             Assert.Throws<ArgumentOutOfRangeException>(() => Numbers.InsertNumber(11, 11, 10, 1));
+        }
+
+        [TestCase(12, ExpectedResult = 21)]
+        [TestCase(513, ExpectedResult = 531)]
+        [TestCase(2017, ExpectedResult = 2071)]
+        [TestCase(414, ExpectedResult = 441)]
+        [TestCase(144, ExpectedResult = 414)]
+        [TestCase(1234321, ExpectedResult = 1241233)]
+        [TestCase(1234126, ExpectedResult = 1234162)]
+        [TestCase(3456432, ExpectedResult = 3462345)]
+        [TestCase(10, ExpectedResult = -1)]
+        [TestCase(20, ExpectedResult = -1)]
+
+        public int  FindNextBiggerNumberTest(int startNumber)
+        {
+           //int i = 1325468;
+           int result = Numbers.FindNextBiggerNumber(startNumber);
+           return result;
         }
     }
 }
