f92cf6de10aa5a561b977bce453a7a5ac66e2457
AndreiValuika <andrei.valuika@gmail.com>
2019-11-21 19:52:00 +0300

    + add 4_Day tasks

---
+++ NET.W.2019.Valuika.04/GCDTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace GCDLib.Tests
+{
+    [TestFixture]
+    public class GCDTests
+    {
+        [TestCase(5, 10, 15, 25, ExpectedResult = 5)]
+        [TestCase(12, 18, 36, ExpectedResult = 6)]
+        [TestCase(1044, -1512, 2436, ExpectedResult = 12)]
+        [TestCase(10, 4, ExpectedResult = 2)]
+        [TestCase(12, 0, 36, ExpectedResult = 12)]
+        public int GCD_Test(int a, int b, params int[] array)
+        {
+            int actual = GCD.GetGCD(out int time, a, b, array);
+            Assert.Positive(time);
+            return actual;
+        }
+
+        [TestCase(5, 10, 15, 25, ExpectedResult = 5)]
+        [TestCase(12, 18, 36, ExpectedResult = 6)]
+        [TestCase(1044, -1512, 2436, ExpectedResult = 12)]
+        [TestCase(10, 4, ExpectedResult = 2)]
+        [TestCase(12, 0, 36, ExpectedResult = 12)]
+        public int GCD_Binnary_Test(int a, int b, params int[] array)
+        {
+            int actual = new GCD().GetMultipleGCDBinnary(out int time, a, b, array);
+            Assert.Positive(time);
+            return actual;
+        }
+    }
+}
