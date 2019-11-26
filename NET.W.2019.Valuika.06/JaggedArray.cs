5e2e9dfc15cbb2aca5cbb5a9a3048f6267327b3a
AndreiValuika <andrei.valuika@gmail.com>
2019-11-27 01:07:09 +0300

    Add Task6

---
+++ NET.W.2019.Valuika.06/JaggedArray.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace SortJaggedArray
+{
+    public static class JaggedArray
+    {
+        private static void SwapRows(ref int[] first, ref int[] second)
+        {
+            int[] temp = first;
+            first = second;
+            second = temp;
+        }
+
+        private static int SumRow(int[] row)
+        {
+            int sum = 0;
+            foreach (var item in row)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+
+        private static bool CompRowsSum(int[] first, int[] second)
+        {
+            return SumRow(first) > SumRow(second) ? true : false;
+        }
+
+        private static bool CompRowsMin(int[] first, int[] second)
+        {
+            int minFirst = first[0];
+            int minSecond = second[0];
+
+            foreach (var item in first)
+            {
+                if (minFirst > item)
+                {
+                    minFirst = item;
+                }
+            }
+
+            foreach (var item in second)
+            {
+                if (minSecond > item)
+                {
+                    minSecond = item;
+                }
+            }
+
+            return minFirst > minSecond ? true : false;
+        }
+
+        static bool CompRowsMax(int[] first, int[] second)
+        {
+            int maxFirst = first[0];
+            int maxSecond = second[0];
+
+            foreach (var item in first)
+            {
+                if (maxFirst < item)
+                {
+                    maxFirst = item;
+                }
+            }
+
+            foreach (var item in second)
+            {
+                if (maxSecond < item)
+                {
+                    maxSecond = item;
+                }
+            }
+
+            return maxFirst > maxSecond ? true : false;
+        }
+
+        public static void SortF(int[][] array, bool invert)
+        {
+            SortFirst(array, invert, CompRowsSum);
+        }
+        public static void SortMin(int[][] array, bool invert)
+        {
+            SortFirst(array, invert, CompRowsMin);
+        }
+        public static void SortMax(int[][] array, bool invert)
+        {
+            SortFirst(array, invert, CompRowsMax);
+        }
+        public static void SortFirst(int[][] array, bool invert, Func<int[], int[], bool> Comp)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (Comp(array[j], array[j + 1]) ^ invert)
+                    {
+                        SwapRows(ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ NET.W.2019.Valuika.06/JaggedArrayTests.cs
@@ -0,0 +1,110 @@
+﻿using NUnit.Framework;
+using SortJaggedArray;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortJaggedArray.Tests
+{
+    [TestFixture()]
+    public class JaggedArrayTests
+    {
+        int[][] _test; 
+        [SetUp]
+        public void Init() 
+        {
+            _test = new int[4][]
+            {
+                new int[] { 10, 20, 30, 40, 50, 60 },
+                new int[] { 11, 21, 31 },
+                new int[] { 12, 22, 32, 42, 52, 62 },
+                new int[] { 100 }
+            };
+        }
+        [Test]
+        public void SortFTest()
+        {
+           int[][]  expend = new int[4][]
+           {
+               new int[] { 11, 21, 31 },
+               new int[] { 100 },
+               new int[] { 10, 20, 30, 40, 50, 60 },
+               new int[] { 12, 22, 32, 42, 52, 62 },
+           };
+           JaggedArray.SortF(_test, false);
+           Assert.AreEqual(_test,expend);
+        }
+        [Test]
+        public void SortFTestInvert()
+        {
+            int[][] expend = new int[4][]
+            {
+               new int[] { 12, 22, 32, 42, 52, 62 },
+               new int[] { 10, 20, 30, 40, 50, 60 },
+               new int[] { 100 },
+               new int[] { 11, 21, 31 },
+            };
+            JaggedArray.SortF(_test, true);
+            Assert.AreEqual(_test, expend);
+        }
+
+        [Test]
+        public void SortMinTest()
+        {
+            int[][] expend = new int[4][]
+            {
+               new int[] { 10, 20, 30, 40, 50, 60 },
+               new int[] { 11, 21, 31 },
+               new int[] { 12, 22, 32, 42, 52, 62 },
+               new int[] { 100 },
+            };
+            JaggedArray.SortMin(_test, false);
+            Assert.AreEqual(_test, expend);
+        }
+
+        [Test]
+        public void SortMinTestInvert()
+        {
+            int[][] expend = new int[4][]
+            {
+               new int[] { 100 },
+               new int[] { 12, 22, 32, 42, 52, 62 },
+               new int[] { 11, 21, 31 },
+               new int[] { 10, 20, 30, 40, 50, 60 },
+            };
+
+            JaggedArray.SortMin(_test, true);
+            Assert.AreEqual(_test, expend);
+        }
+
+        [Test]
+        public void SortMaxTest()
+        {
+            int[][] expend = new int[4][]
+            {
+               new int[] { 11, 21, 31 },
+               new int[] { 10, 20, 30, 40, 50, 60 },
+               new int[] { 12, 22, 32, 42, 52, 62 },
+               new int[] { 100 },
+            };
+            JaggedArray.SortMax(_test, false);
+            Assert.AreEqual(_test, expend);
+        }
+
+        [Test]
+        public void SortMaxTestInvert()
+        {
+            int[][] expend = new int[4][]
+            {
+               new int[] { 100 },
+               new int[] { 12, 22, 32, 42, 52, 62 },
+               new int[] { 10, 20, 30, 40, 50, 60 },
+               new int[] { 11, 21, 31 },
+            };
+            JaggedArray.SortMax(_test, true);
+            Assert.AreEqual(_test, expend);
+        }
+    }
+}
