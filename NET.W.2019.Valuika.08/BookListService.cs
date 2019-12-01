e24c1b62d7d502879f10be848150b223e5bb5b4f
AndreiValuika <andrei.valuika@gmail.com>
2019-12-01 21:31:21 +0300

    add Day_8 Task

---
+++ NET.W.2019.Valuika.08/BookListService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookLib
+{
+    public class BookListService
+    {
+        private BookListStorage _bookStorage;
+        private ICollection<Book> _books;
+
+        public BookListService(BookListStorage bookListStorage)
+        {
+            _bookStorage = bookListStorage;
+            _books = bookListStorage.GetAll();
+        }
+
+        public void AddBook(Book book)
+        {
+            foreach (var item in _books)
+            {
+                if (item.Equals(book))
+                {
+                    throw new ArgumentException("Already exists");
+                }
+            }
+
+            _bookStorage.Add(book);
+        }
+
+        public void RemoveBook(Book book)
+        {
+            if (!_bookStorage.Remove(book))
+            {
+                throw new ArgumentException("Not found");
+            }
+        }
+
+        public ICollection<Book> Sort(IComparer<Book> comparer)
+        {
+            ((List<Book>)_books).Sort(comparer);
+            return _books;
+        }
+
+        public Book FindByTag(Filter filter)
+        {
+            foreach (var item in _bookStorage.GetAll())
+            {
+                if (filter.IsEquals(item))
+                {
+                    return item;
+                }
+            }
+
+            throw new ArgumentException("Not found");
+        }
+
+        public ICollection<Book> GetAll()
+        {
+            return _books;
+        }
+
+        public void Save()
+        {
+            _bookStorage.Save();
+        }
+
+        public void Load()
+        {
+            _bookStorage.Load();
+        }
+    }
+}
---
+++ NET.W.2019.Valuika.08/Filter.cs
@@ -0,0 +1,80 @@
+﻿namespace BookLib
+{
+    public class Filter
+    {
+        public Filter(string iSBN = "",
+            string author = "",
+            string title = "",
+            string publisher = "",
+            int year = -1,
+            int pages = -1,
+            float price = -1)
+        {
+            ISBN = iSBN;
+            Author = author;
+            Title = title;
+            Publisher = publisher;
+            Year = year;
+            Pages = pages;
+            Price = price;
+        }
+
+        public string ISBN { get; set; }
+
+        public string Author { get; set; }
+
+        public string Title { get; set; }
+
+        public string Publisher { get; set; }
+
+        public int Year { get; set; }
+
+        public int Pages { get; set; }
+
+        public float Price { get; set; }
+
+        public bool IsEquals(Book book)
+        {
+            if (!string.IsNullOrEmpty(this.ISBN) &&
+                !this.ISBN.Equals(book.ISBN))
+            {
+                return false;
+            }
+
+            if (!string.IsNullOrEmpty(this.Author) &&
+                !this.Author.Equals(book.Author))
+            {
+                return false;
+            }
+
+            if (!string.IsNullOrEmpty(this.Title) &&
+                !this.Title.Equals(book.Title))
+            {
+                return false;
+            }
+
+            if (!string.IsNullOrEmpty(this.Publisher) &&
+                !this.Publisher.Equals(book.Publisher))
+            {
+                return false;
+            }
+
+            if ((this.Year > 0) && this.Year != book.Year)
+            {
+                return false;
+            }
+
+            if ((this.Pages > 0) && this.Pages != book.Pages)
+            {
+                return false;
+            }
+
+            if ((this.Price > 0) && this.Price != book.Price)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
