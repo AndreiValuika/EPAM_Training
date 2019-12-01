6bbef6836fa3adfa4e038213fee8c41e3f459a7a
AndreiValuika <andrei.valuika@gmail.com>
2019-12-01 21:28:20 +0300

    Add Task2

---
+++ NET.W.2019.Valuika.08/AccountConsoleTest/Program.cs
@@ -0,0 +1,81 @@
+﻿using AccountLib;
+using AcountLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AccountStorage  storage = new AccountStorage("Bank.bin");
+            AccountService service = new AccountService(storage);
+
+            service.AddAccount(new Account("11111", "Name1", "Name11", 100, 50, AccType.Base));
+            service.AddAccount(new Account("22222", "Name2", "Name22", 1000, 4, AccType.Platinum));
+            service.AddAccount(new Account("33333", "Name3", "Name22", 10, 65, AccType.Gold));
+            service.AddAccount(new Account("44444", "Name4", "Name22", 10m, 88, AccType.Base));
+
+            Console.WriteLine("All account :");
+            foreach (var item in service.GetAllAccount())
+            {
+                Console.WriteLine(item + "\n\r");
+            }
+
+
+
+            Console.WriteLine("Try add exist account:");
+            try
+            {
+                service.AddAccount(new Account("33333", "Name3", "Name22", 100m, 5, AccType.Gold));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+               
+            }
+
+            Console.WriteLine("Try delete non-exist account:");
+            try
+            {
+                service.DeleteAccount("8888");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+
+            Console.WriteLine(" Delete  account id=33333 :"+"\n\r");
+            service.DeleteAccount("33333");
+            foreach (var item in service.GetAllAccount())
+            {
+                Console.WriteLine(item + "\n\r");
+            }
+
+            Console.WriteLine(" Add 50 to  account id=22222 :" + "\n\r");
+            service.AddMoney("22222", 50);
+            foreach (var item in service.GetAllAccount())
+            {
+                Console.WriteLine(item + "\n\r");
+            }
+
+            Console.WriteLine("Save. And Load to other storage: \n\r");
+            service.Save();
+            storage = new AccountStorage("Bank.bin");
+            service = new AccountService(storage);
+            service.Load();
+            foreach (var item in service.GetAllAccount())
+            {
+                Console.WriteLine(item + "\n\r");
+            }
+            Console.WriteLine(  );
+            
+
+        }
+    }
+}
---
+++ NET.W.2019.Valuika.08/AcountLib/Account.cs
@@ -0,0 +1,44 @@
+﻿using AcountLib;
+using System;
+
+namespace AccountLib
+{
+    public class Account
+    {
+        public string  Id { get; set; }
+        public string OwnerFirstName { get; set; }
+        public string OwnerLastName { get; set; }
+        public decimal Amount { get; set; }
+        public int Points { get; set; }
+        public AccType Type { get; set; }
+
+
+        public Account(string id, string ownerFirstName, string ownerLastName, decimal amount, int points, AccType type)
+        {
+            Id = id;
+            OwnerFirstName = ownerFirstName;
+            OwnerLastName = ownerLastName;
+            Amount = amount;
+            Points = points;
+            Type = type;
+        }
+
+        public Account()
+        {
+        }
+
+       
+        public override string ToString()
+        {
+            return String.Format("Account №{0} Owner: {1} {2}  Amount: {3}$  points:{4}   Type: {5}",
+                Id, OwnerFirstName,
+                OwnerLastName, Amount, Points, Type);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Account account &&
+                   Id == account.Id;
+        }
+    }
+}
